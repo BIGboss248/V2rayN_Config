3ea53a74c20cb73e949b0379494a83daef6547f1
BIGboss248 <aminjamali79@gmail.com>
2023-04-13 18:38:56 +0430

    2 catagories

--- Routing_Rules.js
+++ Routing_Rules.js
@@ -1,6 +1,29 @@
 [
   {
-    "id": "4880784328903101967",
+    "id": "4994734128742020854",
+    "port": "",
+    "outboundTag": "direct",
+    "ip": [],
+    "domain": [
+      "domain:discord.com",
+      "domain:discord.media",
+      "google.com",
+      "console.hetzner.cloud",
+      "domain:ubisoft.com",
+      "domain:alert-eu.battle.net",
+      "domain:blizzard.com",
+      "domain:telemetry-in.battle.net",
+      "domain:ubi.com",
+      "domain:geforce.com",
+      "domain:nvidia.com",
+      "domain:openai.com",
+      "dota2.com",
+      "domain:steamserver.net"
+    ],
+    "enabled": true
+  },
+  {
+    "id": "5743518602487990311",
     "port": "",
     "outboundTag": "direct",
     "ip": [],
@@ -24,55 +47,12 @@
       "fardadshabake.com",
       "neshan.org",
       "dayachange.com",
-      "rahavad365.com",
-      "boghrat.com",
-      "iranxia.net",
-      "masterkala.com",
-      "api.torob.com",
-      "dlfox.com",
-      "par30games.net"
+      "rahavad365.com"
     ],
     "enabled": true
   },
   {
-    "id": "5025892071271854078",
-    "port": "",
-    "outboundTag": "direct",
-    "ip": [],
-    "domain": [
-      "domain:anydesk.com",
-      "domain:discord.com",
-      "domain:discord.media",
-      "google.com",
-      "maps.google.com",
-      "domain:googleapis.com",
-      "console.hetzner.cloud",
-      "10fastfingers.com",
-      "mi.com",
-      "bing.com"
-    ],
-    "enabled": true
-  },
-  {
-    "id": "5076198951147597584",
-    "port": "",
-    "outboundTag": "direct",
-    "ip": [],
-    "domain": [
-      "domain:ubisoft.com",
-      "domain:alert-eu.battle.net",
-      "domain:blizzard.com",
-      "domain:telemetry-in.battle.net",
-      "domain:ubi.com",
-      "domain:geforce.com",
-      "domain:nvidia.com",
-      "domain:openai.com",
-      "udemy.com"
-    ],
-    "enabled": true
-  },
-  {
-    "id": "4938944873475307635",
+    "id": "4673530010386343830",
     "port": "0-65535",
     "outboundTag": "proxy",
     "ip": [],
